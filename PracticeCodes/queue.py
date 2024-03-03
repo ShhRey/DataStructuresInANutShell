464c8b6a2bcbf0dc70b496d321952ab657d69df2
Shrey Trivedi <shreytrivedi577@gmail.com>
2024-03-03 08:35:49 -0500

    Understanding DS along with Execution

---
+++ PracticeCodes/queue.py
@@ -0,0 +1,65 @@
+class Queue:
+    def __init__(self, max_size):
+        self.queue = []
+        self.max_size = max_size
+       
+    # Function for Pushing Element into the Queue
+    def enqueue(self, item):
+        if len(self.queue) < self.max_size:
+            print(f"Inserting {item} into Queue")
+            return self.queue.append(item)
+        else:
+            print(f"Queue is Full and cannot hold more than {self.max_size} items")
+    
+
+    # Function for Poping Element out of Queue
+    def dequeue(self):
+        if len(self.queue) > 0:
+            print(f"Will Pop {self.queue[0]} from Queue")
+            return self.queue.pop(0)
+        else:
+            print("Queue Already Empty") 
+    
+
+    # Function for checking if Queue is Empty or Not
+    def is_empty(self):
+        if len(self.queue) == 0:
+            print("Queue is Empty")
+            return not self.queue
+        else:
+            print("Queue Not Empty") 
+        
+    
+    # Function for checking if Queue is Full or Not
+    def is_full(self):
+        if len(self.queue) > self.max_size:
+            print(f"Queue is Full after inserting {self.max_size} items !")
+        else:
+            space = self.max_size - len(self.queue)
+            print(f"{space} Elements can be added before Queue is Full")
+
+        
+    # Function for returning the Size of the Queue
+    def size(self):
+        print(f"There are {len(self.queue)} Elements in Queue")
+        return len(self.queue)
+        
+
+queue = Queue(5)
+queue.is_empty()
+queue.enqueue('1')
+queue.enqueue('2')
+queue.enqueue('3')
+queue.enqueue('4')
+queue.is_full()
+queue.enqueue('5')
+queue.enqueue('6')
+queue.is_empty()
+queue.dequeue()
+queue.dequeue()
+queue.dequeue()
+queue.dequeue()
+queue.dequeue()
+queue.dequeue()
+queue.dequeue()
+queue.dequeue()
---
+++ PracticeCodes/stack.py
@@ -0,0 +1,64 @@
+class Stack:
+    def __init__(self, max_size):
+        self.stack = []
+        self.max_size = max_size
+
+
+    # Function for Pushing Element into the Stack
+    def push(self, item):
+        if len(self.stack) < self.max_size:
+            print(f"Inserting {item} into Stack")
+            return self.stack.append(item)
+        else:
+            print(f"Stack is Full and cannot hold more than {self.max_size} items")
+
+
+    # Function for Poping Element out of Stack
+    def pop(self):
+        if len(self.stack) > 0:
+            print(f"Will Pop {self.stack[-1]} from Stack")
+            return self.stack.pop()
+        else:
+            print("Stack Already Empty")
+
+
+    # Function for Peeking into the Stack
+    def peek(self):
+        print(f"The Last Element in Stack: {self.stack[-1]}")
+        return self.stack[-1]
+
+
+    # Function to check if Stack is Empty or Not
+    def is_empty(self):
+        if len(self.stack) == 0:
+            print("Stack is Empty")
+            return not self.stack
+        else:
+            print("Stack Not Empty")
+    
+
+    # Function to check if Stack is Full or Not
+    def is_full(self):
+        if len(self.stack) > self.max_size:
+            print(f"Stack is Full after inserting {self.max_size} items !")
+        else:
+            space = self.max_size - len(self.stack)
+            print(f"{space} Elements can be added before Stack is Full")
+
+    # Function for returning the Size of the Stack
+    def size(self):
+        print(f"There are {len(self.stack)} Elements in Stack")
+        return len(self.stack)
+
+
+stack = Stack(2)
+stack.pop()
+stack.push('apple')
+stack.push('banana')
+stack.push('cherry')
+stack.peek()
+stack.size()
+stack.pop()
+stack.pop()
+stack.pop()
+stack.pop()

2fa92bcb7b5fcf1bb6813939b442c42304ec4eee
Shrey Trivedi <shreytrivedi577@gmail.com>
2024-03-08 00:28:56 -0500

    Beginner Guide Questions

--- LeetCode/876-MiddleOfLinkedList.py
+++ LeetCode/876-MiddleOfLinkedList.py
@@ -3,11 +3,47 @@ class Node:
     def __init__(self, item):
         self.item = item
         self.next = None
-        self.prev = None
 
-
-# Class to Represent Linked List
-class LinkedList:
-    def __init__(self):
+# Class to represent Singly Linked List
+class SinglyLL:
+    def __init__(self) -> None:
         self.head = None
-        self.tail = None
+
+    # Function to Insert Elements after Head
+    def insertAtEnd(self, item):
+        new_node = Node(item)
+        if not self.head:
+            self.head = new_node
+            return
+        current_node = self.head
+        while current_node.next:
+            current_node = current_node.next
+        current_node.next = new_node
+
+    # Function to find Middle Node
+    def middleNode(self):
+        slowPointer = self.head
+        fastPointer = self.head
+        while (fastPointer is not None) and (fastPointer.next is not None):
+            slowPointer = slowPointer.next
+            fastPointer = fastPointer.next.next
+        # Fetching all the Nodes after Middle to store in result
+        result = []
+        current_node = slowPointer
+        while current_node:
+            result.append(current_node.item)
+            current_node = current_node.next
+        return result
+
+# Collecting Inputs from Users
+arr = []
+elementCount = int(input("Enter Total Elements you want to Add: "))
+for i in range(1, elementCount+1):
+    element = int(input(f"Adding Element {i} into Head: "))
+    arr.append(element)
+
+sll = SinglyLL()
+for i in arr:
+    sll.insertAtEnd(i)
+
+print(sll.middleNode())
